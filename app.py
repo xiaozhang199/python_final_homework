1047bca906d0932b0b3c84c15c8f11131872e1cf
xiaozhang199 <75928805+xiaozhang199@users.noreply.github.com>
2022-05-23 15:01:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-from flask import Flask
-from flask import render_template
-import jinja2
-import pymysql
 import sys
 sys.path.append('d:\python\lib\site-packages')
 from wordcloud import WordCloud
@@ -9,150 +5,39 @@ import jieba
 from matplotlib import pyplot as plt#绘图，数据可视化
 from PIL import Image#图片处理
 import numpy as np#矩阵运算
+import pymysql
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route('/index')
-def index():
-    return render_template("index.html")
-
-@app.route("/list")
-def movie():
-    datalist = []
-    link = pymysql.connect(
+link = pymysql.connect(
         host='localhost',
         port=3306,
         user='root',
         password='zyx18080623585',
         database='crawldata',
         charset='utf8mb4')
-    cur = link.cursor()
-    sql = "select * from up_infomation"
-    cur.execute(sql)  # 这里不是运行出来的文件，是运行的行数
-    data = cur.fetchall()
-    for item in data:
-        datalist.append(list(item))
-    cur.close()
-    link.close()
-    return render_template("list.html", videos=datalist)
+cur = link.cursor()
+sql = "SELECT title FROM up_infomation"
+cur.execute(sql)  # 这里不是运行出来的文件，是运行的行数
+data = cur.fetchall()
+text = ""
+for title in data:
+    text += str(title[0])
+cur.close()
+link.close()
 
-@app.route('/analysis')
-def analysis():
-    return render_template("analysis.html")
+cut = jieba.cut(text)
+content = ' '.join(cut)
 
+img = Image.open(r'.\static\assets\images\cloud.jpg') #打开图片
+img_array = np.array(img)#将图片转换为图片数组
+wc = WordCloud(
+        background_color = "white",
+        mask = img_array,
+        font_path = "STFANGSO.TTF"
+).generate_from_text(content)
 
-@app.route('/comment')
-def comment():
-    datalist = []
-    link = pymysql.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='zyx18080623585',
-        database='crawldata',
-        charset='utf8mb4')
-    cur = link.cursor()
-    sql = "SELECT COUNT(*) FROM up_infomation WHERE comment_num < 1000"
-    cur.execute(sql)  
-    data = cur.fetchall()
-    for nums in data:
-        for num in nums:
-            datalist.append(num)
-    sql = "SELECT COUNT(*) FROM up_infomation WHERE comment_num between 1000 and 10000"
-    cur.execute(sql)
-    data = cur.fetchall()
-    for nums in data:
-        for num in nums:
-            datalist.append(num)
-    sql = "SELECT COUNT(*) FROM up_infomation WHERE comment_num between 10000 and 100000"
-    cur.execute(sql)
-    data = cur.fetchall()
-    for nums in data:
-        for num in nums:
-            datalist.append(num)
-    sql = "SELECT COUNT(*) FROM up_infomation WHERE comment_num >= 100000"
-    cur.execute(sql)
-    data = cur.fetchall()
-    for nums in data:
-        for num in nums:
-            datalist.append(num)
-    cur.close()
-    link.close()
-    return render_template("comment.html",datalist=datalist)
+# #绘制图片
+plt.imshow(wc)#其中wc为要处理的图像及数组，处理图像，对传入的数组或者图像进行处理并显示格式
+plt.axis('off') #不显示坐标轴
 
-
-@app.route('/view')
-def view():
-    datalist = []
-    link = pymysql.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='zyx18080623585',
-        database='crawldata',
-        charset='utf8mb4')
-    cur = link.cursor()
-    sql = "SELECT COUNT(*) FROM up_infomation WHERE view_num < 100000"
-    cur.execute(sql)  
-    data = cur.fetchall()
-    for nums in data:
-        for num in nums:
-            datalist.append(num)
-    sql = "SELECT COUNT(*) FROM up_infomation WHERE view_num between 100000 and 300000"
-    cur.execute(sql)
-    data = cur.fetchall()
-    for nums in data:
-        for num in nums:
-            datalist.append(num)
-    sql = "SELECT COUNT(*) FROM up_infomation WHERE view_num between 300000 and 500000"
-    cur.execute(sql)
-    data = cur.fetchall()
-    for nums in data:
-        for num in nums:
-            datalist.append(num)
-    sql = "SELECT COUNT(*) FROM up_infomation WHERE view_num between 500000 and 1000000"
-    cur.execute(sql)
-    data = cur.fetchall()
-    for nums in data:
-        for num in nums:
-            datalist.append(num)
-    sql = "SELECT COUNT(*) FROM up_infomation WHERE view_num > 1000000"
-    cur.execute(sql)
-    data = cur.fetchall()
-    for nums in data:
-        for num in nums:
-            datalist.append(num)
-    cur.close()
-    link.close()
-    return render_template("view.html",datalist=datalist)
-
-@app.route('/share')
-def share():
-    datalist = []
-    link = pymysql.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='zyx18080623585',
-        database='crawldata',
-        charset='utf8mb4')
-    cur = link.cursor()
-    sql = "select * from up_infomation ORDER BY share_num DESC LIMIT 10"
-    cur.execute(sql)  
-    data = cur.fetchall()
-    for item in data:
-        datalist.append(list(item))
-    cur.close()
-    link.close()
-    return render_template("list.html", videos=datalist)
-
-@app.route('/word')
-def word():
-    return render_template("word.html")
-
-if __name__ == '__main__':
-    app.run()
+# plt.show()#显示生成的词云图片
+plt.savefig(r'.\static\assets\images\word.jpg',dpi = 800)#dpi是分辨率，默认分辨率有点低，要调高
